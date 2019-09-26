6d8ed5f5f518510581096104161749dca368a5f1
Andrew Waller <andrewhwaller@gmail.com>
2019-09-26 15:42:01 +0000

    Automatically backed up by Learn

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -2,7 +2,19 @@
 import React from 'react';
 
 class YouTubeDebugger extends React.Component {
-
+  constructor() {
+    super()
+    this.state = {
+      errors: [],
+      user: null,
+      settings: {
+        bitrate: 8,
+        video: {
+          resolution: '1080p'
+        }
+      }
+    }
+  }
 }
 
 export default YouTubeDebugger;

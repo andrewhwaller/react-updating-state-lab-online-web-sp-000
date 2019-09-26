c5bc7735234d3a206d7aa00c63c1935245e7a468
Andrew Waller <andrewhwaller@gmail.com>
2019-09-26 15:50:50 +0000

    Automatically backed up by Learn

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -3,7 +3,8 @@ import React from 'react';
 
 class YouTubeDebugger extends React.Component {
   constructor() {
-    super()
+    super();
+
     this.state = {
       errors: [],
       user: null,

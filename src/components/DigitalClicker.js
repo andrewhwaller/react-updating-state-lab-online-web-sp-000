6987e4e6fedbb6e5d50d8ed79af321dcf22ccfce
Andrew Waller <andrewhwaller@gmail.com>
2019-09-26 15:24:12 +0000

    Automatically backed up by Learn

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -2,5 +2,23 @@
 import React from 'react';
 
 class DigitalClicker extends React.Component {
+  constructor() {
+    super();
 
+    this.state = {
+      timesClicked: 0.
+    };
+  }
+
+  handleClick = () => {
+    this.setState(prevState => ({
+      timesClicked: prevState.timesClicked+1
+    }))
+  }
+
+  render() {
+    return(
+
+    )
+  }
 }
